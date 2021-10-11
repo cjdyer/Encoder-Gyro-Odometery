7c51b690b575f3c980a316e8397f603cc31c9006
cjdyer <cjdyer0@gmail.com>
2021-10-11 20:38:23 +1300

    Added documentation

--- Odometery.cpp
+++ Odometery.cpp
@@ -1,10 +1,7 @@
 #include <cmath>
 #include "Odometery.h"
 
-Odometery::Odometery(const uint8_t _id) : 
-    id_(_id)
-{
-}
+Odometery::Odometery() { }
 
 uint8_t Odometery::GetID() { return id_; }
 double Odometery::GetX() {return global_x_; }
@@ -14,24 +11,28 @@ template <typename EncoderUnits>
 void Odometery::CalculatePosition(const EncoderUnits _left_encoder,const EncoderUnits _right_encoder, const EncoderUnits _tracking_encoder, const float _gyroscope)
 {
     static EncoderUnits 
-    local_vertical_position, local_horizontal_position, 
+    local_vertical_position, local_horizontal_position,
     change_x, change_y, 
     prev_local_horizontal_position, prev_local_vertical_position;
-    static double global_change_x, global_change_y, gyroscope_radians;
+    static double global_change_x, global_change_y, 
+    gyroscope_radians, sin_gyroscope, cos_gyroscope;
 
-    local_vertical_position = (_left_encoder + _right_encoder) / 2;
+    local_vertical_position = (_left_encoder + _right_encoder) / 2; // Calculate the local position relative to the current heading
     local_horizontal_position = _tracking_encoder;
 
-    change_x = local_horizontal_position - prev_local_horizontal_position;
-    change_y = local_vertical_position - prev_local_vertical_position;
+    change_x = local_horizontal_position - prev_local_horizontal_position; // Calculate the change in position between the current 
+    change_y = local_vertical_position - prev_local_vertical_position;     // and previous cycle
 
     prev_local_horizontal_position = local_horizontal_position;
     prev_local_vertical_position = local_vertical_position;
 
-    gyroscope_radians = _gyroscope * degrees_to_radians_;
-    global_change_x = change_y * sin(gyroscope_radians) - change_x * cos(gyroscope_radians);
-	global_change_y = change_x * sin(gyroscope_radians) - change_y * cos(gyroscope_radians);
+    gyroscope_radians = _gyroscope * degrees_to_radians_; // Reduced repeated calculations
+    sin_gyroscope = sin(gyroscope_radians);
+    cos_gyroscope = cos(gyroscope_radians);
 
-    global_x_ += global_change_x;
+    global_change_x = change_y * sin_gyroscope - change_x * cos_gyroscope; // Transform the local movement into the global space
+	global_change_y = change_x * sin_gyroscope - change_y * cos_gyroscope;
+
+    global_x_ += global_change_x; // Add position change to current position
     global_y_ += global_change_y;
 }
--- Odometery.h
+++ Odometery.h
@@ -3,19 +3,59 @@
 
 #include <stdint.h>
 
+/**
+ * Implementation of odometery in C++
+ *
+ * Given the following three encoders and a heading it will calculate a rough cartesian position.
+ * 
+ * Information required:
+ * 
+ * Left Encoder
+ * Right Encoder
+ * Tracking/Horizontal Encoder
+ * Heading Angle (Gyroscope)
+ * 
+ * This code should not be used as an APS (absolute positioning system), but can be used to
+ * give a rough robot position for use with other sensor information or robot data.
+ *
+ */
 class Odometery
 {
 public:
-    Odometery(const uint8_t _id);
-    uint8_t GetID();
+    /**
+     * @brief Creates an odometery object
+     */
+    Odometery();
+
+    /**
+     * @brief Gets the estimated X position of the robot
+     * 
+     * @return The X positon of the robot
+     */
     double GetX();
+
+    /**
+     * @brief Gets the estimated Y position of the robot
+     * 
+     * @return The Y positon of the robot
+     */
     double GetY();
+
+    /**
+     * @brief Calculate the position of the robot
+     * 
+     * @tparam The units used when interfacing with the encoders, likely int32_t 
+     * @param _left_encoder The left encoder value
+     * @param _right_encoder The right encoder value
+     * @param _tracking_encoder The horizontal/tracking encoder value
+     * @param _gyroscope The gyroscope value, assumed 1 deg = 1.0
+     */
     template <typename EncoderUnits>
     void CalculatePosition(const EncoderUnits _left_encoder,const EncoderUnits _right_encoder, const EncoderUnits _tracking_encoder, const float _gyroscope);
-private:
-    const uint8_t id_;
 
-    double global_x_, global_y_;
+private:
+
+    double global_x_, global_y_; // The global X/Y position
 
     static constexpr double degrees_to_radians_ = 3.14159265358979323846 / 180; // pi / 180
 };
